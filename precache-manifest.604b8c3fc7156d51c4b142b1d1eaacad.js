929c0b47de7e042c94c96b4c1dfb0c0bc44b2174
Hallvard Jore Christensen <hallvard.jore.christensen@gmail.com>
2020-09-26 19:35:12 +0200

    deploy

---
+++ precache-manifest.604b8c3fc7156d51c4b142b1d1eaacad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b917ef31c3a1a1c13e7d",
+    "url": "/list-pwa/css/app.5083492d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/list-pwa/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0273328981bf3729df20812102d847fa",
+    "url": "/list-pwa/index.html"
+  },
+  {
+    "revision": "3a95b95089e0c55c3b70",
+    "url": "/list-pwa/js/about.757b7a8f.js"
+  },
+  {
+    "revision": "b917ef31c3a1a1c13e7d",
+    "url": "/list-pwa/js/app.2b42ab1d.js"
+  },
+  {
+    "revision": "e8773d96a4b7c30357f5",
+    "url": "/list-pwa/js/chunk-vendors.12d21173.js"
+  },
+  {
+    "revision": "c02cffeda49b2c9aae940647299a5357",
+    "url": "/list-pwa/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/list-pwa/robots.txt"
+  }
+]);
